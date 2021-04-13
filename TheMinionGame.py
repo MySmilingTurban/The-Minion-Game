f1923471410dc57f1184977bf484e42e88380290
Gurmehar Singh Khalsa <gurmeharsinghkhalsa1005@gmail.com>
2021-04-14 00:22:06 +0530

    Update TheMinionGame.py

--- TheMinionGame.py
+++ TheMinionGame.py
@@ -1,23 +1,25 @@
-def minion_game(string):
-    n1='Stuart' #chooses substring starting with consonants
+def minion_game(string,name1='Stuart',name2='Kevin'):
+    n1=name1 #chooses substring starting with consonants
     p1=0        #points of Stuart
-    n2='Kevin'  #chooses substring starting with vowels
+    n2=name2  #chooses substring starting with vowels
     p2=0        #points of Kevin
     v=['A','E','I','O','U']
-#Combinations of each substring's occurence = len(string) - (index of substring)
+    #Combinations of each substring's occurence = len(string) - (index of substring)
     for z in range(len(string)):
         if string[z] in v:      #substring starting with a vowel
             p2+=len(string)-z
         else:                   #substring starting with a consonant
             p1+=len(string)-z 
-#Comparison of Scores for Result
+    #Comparison of Scores for Result
     if p1>p2:
-        print(n1,p1,sep=' ')    #Stuart Wins
+        print(n1,'Wins, scoring',p1,sep=' ')   
     elif p1<p2:
-        print(n2,p2,sep=' ')    #Kevin Wins
+        print(n2,'Wins, scoring',p2,sep=' ')    
     else:
         print('Draw')
     
 if __name__ == '__main__':
     s = input()
-    minion_game(s)
+    s1=input()
+    s2=input()
+    minion_game(s,s1,s2)
